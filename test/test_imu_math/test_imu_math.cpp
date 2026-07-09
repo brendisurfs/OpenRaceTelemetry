a97de784970d92cd5b0a0a25cb2f87ea8de5a300
Brendan P. <64713032+brendisurfs@users.noreply.github.com>
2026-07-09 14:45:44 -0700

    feat(imu_test): integrated actual test for temps

--- test/test_imu_math/test_imu_math.cpp
+++ test/test_imu_math/test_imu_math.cpp
@@ -1,11 +1,20 @@
 #include <unity.h>
 
+#include "imu_math.h"
 #include "unity_internals.h"
 
 void setUp(void) {}
 void tearDown(void) {}
 
+void test_converts_temp() {
+  float expected = 29.33;
+  float actual = convert_temp(-2448);
+
+  UNITY_TEST_ASSERT_EQUAL_FLOAT(expected, actual, 14, "Temp should be a float");
+}
+
 int main(int argc, char** argv) {
   UNITY_BEGIN();
+  test_converts_temp();
   UNITY_END();
 }
