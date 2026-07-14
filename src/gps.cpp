1b94aa86285f2ee27aeef36f52a21619e304e1ee
Brendan P. <64713032+brendisurfs@users.noreply.github.com>
2026-07-14 01:41:50 -0700

    fix(gps): bounds check and proper initializiation of fields

--- src/gps.cpp
+++ src/gps.cpp
@@ -7,18 +7,15 @@
  * NMEA messages are ASCII printable messages with a maximum of 82 chars.
  */
 NmeaMessage parse_nmea_message(const char* nmea_message, size_t length) {
-  char talker[2];
-  char message_type[3];
+  NmeaMessage result{};
+  if (length < 6 || nmea_message[0] != '$') {
+    result.valid = false;
+    return result;
+  }
 
   // copy talker to buffer
-  memcpy(talker, &nmea_message[1], 2 * sizeof(nmea_message[0]));
-
-  // copy message type
-  memcpy(message_type, &nmea_message[3], 3 * sizeof(nmea_message[0]));
-
-  NmeaMessage result{};
-  memcpy(result.talker, talker, sizeof(talker));
-  memcpy(result.message_type, message_type, sizeof(message_type));
+  memcpy(result.talker, &nmea_message[1], 2 * sizeof(nmea_message[0]));
+  memcpy(result.message_type, &nmea_message[3], 3 * sizeof(nmea_message[0]));
   result.valid = true;
 
   return result;
