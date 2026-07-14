8c212bdb176012e7940de5c2bbcde98acaec95fa
Brendan P. <64713032+brendisurfs@users.noreply.github.com>
2026-07-14 01:34:21 -0700

    fix(gps): buffer sizes changed

--- src/gps.cpp
+++ src/gps.cpp
@@ -1,19 +1,25 @@
 #include "gps.h"
 
+#include <cstdio>
 #include <cstring>
 
 /*
  * NMEA messages are ASCII printable messages with a maximum of 82 chars.
  */
 NmeaMessage parse_nmea_message(const char* nmea_message, size_t length) {
-  char talker[3];
-  char message_type[4];
+  char talker[2];
+  char message_type[3];
 
   // copy talker to buffer
-  memcpy(talker, &nmea_message[0], 3 * sizeof(nmea_message[0]));
+  memcpy(talker, &nmea_message[1], 2 * sizeof(nmea_message[0]));
 
   // copy message type
-  memcpy(message_type, &nmea_message[3], 4 * sizeof(nmea_message[0]));
+  memcpy(message_type, &nmea_message[3], 3 * sizeof(nmea_message[0]));
 
-  return {.talker = {*talker}, .message_type = {*message_type}, .valid = true};
+  NmeaMessage result{};
+  memcpy(result.talker, talker, sizeof(talker));
+  memcpy(result.message_type, message_type, sizeof(message_type));
+  result.valid = true;
+
+  return result;
 }
