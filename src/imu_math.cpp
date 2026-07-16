2ef89759e9e98bbec4239e0b97dc957cd445e1e2
Brendan P. <64713032+brendisurfs@users.noreply.github.com>
2026-07-16 16:48:30 -0700

    feat(imu): added roll, pitch, complementary filter functions
    Needed to convert the raw accelerometer data into usable degrees, so
    added a roll and pitch function. also added a complementary filter to
    compensate for drift.
    
    Signed-off-by: Brendan P. <64713032+brendisurfs@users.noreply.github.com>

--- src/imu_math.cpp
+++ src/imu_math.cpp
@@ -1,9 +1,50 @@
 #include "imu_math.h"
 
+#include <cmath>
 #include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <string>
 
-float convert_temp(int16_t temp_raw) { return temp_raw / 340.0f + 36.53f; }
+// LSB per g at +- 2g range
+static const float ACCEL_SCALE_2G = 16384.0f;
+
+void todo(std::string message) {
+  printf("%s\n", message.c_str());
+  exit(1);
+};
+
+float convert_temp(int16_t temp_raw) {
+  return temp_raw / 340.0f + 36.53f;
+}
 
 int16_t combine_bytes(uint8_t high, uint8_t low) {
   return (int16_t)((high << 8 | low));
+}
+
+/**
+ * Roll angle is equal to arctan(Ay/Az)
+ */
+float calculate_roll(int16_t accel_y, int16_t accel_z) {
+  return std::atan2(accel_y, accel_z) * (180.0f / M_PI);
+}
+
+float calculate_pitch(int16_t accel_x) {
+  float accel_x_g = (float)accel_x / ACCEL_SCALE_2G;
+  float clamped_accel_x_g = std::fmax(-1.0f, std::fmin(1.0f, accel_x_g));
+  return std::asin(clamped_accel_x_g) * (180.0f / M_PI);
+}
+
+float complemetary_filter(float accel_angle,
+                          float gyro_rate,
+                          float time_delta) {
+  static float prev_angle = 0.0;
+  static const float alpha = 0.98;
+
+  float new_angle =
+      alpha * (prev_angle + gyro_rate * time_delta) + (1 - alpha) * accel_angle;
+
+  prev_angle = new_angle;
+
+  return roundf(new_angle);
 }
