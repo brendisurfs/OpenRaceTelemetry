1c880921c8bcbf76f80ef0df53c0097ca7af75a5
Brendan P. <64713032+brendisurfs@users.noreply.github.com>
2026-07-29 09:18:54 -0700

    fix(blink): updating blink to stm32

--- src/blink.cpp
+++ src/blink.cpp
@@ -1,11 +1,5 @@
 #include <Arduino.h>
 
-#include "esp32-hal-gpio.h"
-#include "esp32-hal.h"
-#include "hal/gpio_types.h"
-#include "io_pin_remap.h"
-#include "pins_arduino.h"
-
 /**
  * Different flashes for different states of the system.
  */
@@ -13,11 +7,11 @@ enum FlashDelay {
   //  200ms for warming up
   WARMUP = 200,
   // 20 ms flash timeout
-  SUCCESS = 20,
+  FLASH_SUCCESS = 20,
 };
 
 void configure_led(void) {
-  pinMode(LED_BUILTIN, GPIO_MODE_OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void blink_led(void) {
@@ -41,7 +35,7 @@ void gps_connected_blink(void) {
   int max_flashes = 10;
   for (int i = 0; i < max_flashes; i++) {
     digitalWrite(LED_BUILTIN, HIGH);
-    delay(SUCCESS);
+    delay(FLASH_SUCCESS);
     digitalWrite(LED_BUILTIN, LOW);
   }
 }
