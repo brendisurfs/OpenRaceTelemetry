c42270a8002e971a7a8b285f43840805802000c2
Brendan P. <64713032+brendisurfs@users.noreply.github.com>
2026-08-08 18:43:25 -0700

    fix(imu): started converting IMU code to STM32
    very slowly rolling through configuring I2C on the STM32, its much more
    complicated than the Arduino setup, but Im learning what each thing
    does. Kinda dont like that every resource just uses the IDE, id rather
    know what everything is doing.
    
    Signed-off-by: Brendan P.
    <64713032+brendisurfs@users.noreply.github.com>
    Signed-off-by: Brendan P. <64713032+brendisurfs@users.noreply.github.com>

--- src/imu.cpp
+++ src/imu.cpp
@@ -1,13 +1,21 @@
 #include "imu.h"
-
-#include <Arduino.h>
-#include <Wire.h>
+#include "blink.h"
 
 #include <cstddef>
-#include <cstdint>
+#include <cstdio>
 #include <cstdlib>
 
+#include <stm32f4xx_hal.h>
+
 #include "imu_math.h"
+#include "stm32f411xe.h"
+#include "stm32f4xx_hal_cortex.h"
+#include "stm32f4xx_hal_def.h"
+#include "stm32f4xx_hal_gpio.h"
+#include "stm32f4xx_hal_gpio_ex.h"
+#include "stm32f4xx_hal_i2c.h"
+#include "stm32f4xx_hal_rcc.h"
+#include "stm32f4xx_hal_uart.h"
 
 #define MPU_ADDR 0x68
 #define PWR_MGMT_REG 0x6B
@@ -17,11 +25,59 @@
 
 #define READ_BUF_SIZE 14
 
+UART_HandleTypeDef huart1;
+
+void I2C_Error_Handler() {
+  while (1) {
+    i2c_error_blink();
+  }
+}
+
+/*
+ * Enable our GPIO for the I2C bus
+ */
+void configure_i2c_gpio(void) {
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  GPIO_InitTypeDef gpio_config{
+      .Pin = GPIO_PIN_6 | GPIO_PIN_7,
+      .Mode = GPIO_MODE_AF_OD,
+      .Pull = GPIO_NOPULL,
+      .Speed = GPIO_SPEED_FREQ_HIGH,
+      .Alternate = GPIO_AF4_I2C1,
+  };
+
+  HAL_GPIO_Init(GPIOB, &gpio_config);
+}
+
 void configure_i2c_wire_interface() {
-  // Uses the board variant's default I2C pins. On boards without external
-  // pull-up resistors on SDA/SCL, call Wire.setSDA()/Wire.setSCL() before
-  // begin() and enable INPUT_PULLUP on those pins explicitly.
-  Wire.begin();
+  __HAL_RCC_I2C1_CLK_ENABLE();
+
+  I2C_InitTypeDef init_config{
+      .ClockSpeed = 100000,
+      .DutyCycle = I2C_DUTYCYCLE_2,
+
+      // Only if this module were a slave module would we change this
+      .OwnAddress1 = 0,
+
+      // MPU6050 uses 7-bit addressing
+      .AddressingMode = I2C_ADDRESSINGMODE_7BIT,
+
+      // Only relevant for slave mode
+      .DualAddressMode = I2C_DUALADDRESS_DISABLE,
+      .OwnAddress2 = 0,
+      .GeneralCallMode = I2C_GENERALCALL_DISABLE,
+      .NoStretchMode = I2C_NOSTRETCH_DISABLE,
+
+  };
+  I2C_HandleTypeDef handle{};
+  handle.Instance = I2C1;
+  handle.Init = init_config;
+
+  HAL_StatusTypeDef status = HAL_I2C_Init(&handle);
+  if (status != HAL_OK) {
+    I2C_Error_Handler();
+  }
 }
 
 /*
@@ -30,21 +86,7 @@ void configure_i2c_wire_interface() {
  * device address like 0x68/0x69.
  */
 void scan_i2c_bus(void) {
-  Serial.println("Scanning I2C bus...");
-
-  int found = 0;
-  for (uint8_t addr = 0x08; addr <= 0x77; addr++) {
-    Wire.beginTransmission(addr);
-    uint8_t err = Wire.endTransmission();
-    if (err == 0) {
-      Serial.printf("Found I2C device at 0x%02X\n", addr);
-      found++;
-    }
-  }
-
-  if (found == 0) {
-    Serial.printf("No I2C devices found on the bus\n");
-  }
+  // TODO: implement scan.
 }
 
 void wake_mpu() {
@@ -63,17 +105,28 @@ void configure_accel_range() {
 
 // Configure gyroscope range to ±250 °/s
 void configure_gyro() {
+  MPU_Region_InitTypeDef config{};
+  config.Enable = 1;
+
+  // HAL_MPU_ConfigRegion( *MPU_Init)
   Wire.beginTransmission(MPU_ADDR);
   Wire.write(GYRO_CONFIG);  // GYRO_CONFIG register
   Wire.write(0x00);         // ±250dps
   Wire.endTransmission(true);
+
+  uint8_t uart_data[10] = {0};
+  uint16_t len_of_msg = sprintf(uart_data, "hey %s\n", "cuh");
+  HAL_UART_Transmit(&huart1, uart_data, len_of_msg, 100);
 }
 
 void setup_imu() {
-  wake_mpu();
-  configure_gyro();
-  configure_accel_range();
-  Serial.println("MPU6050 initialized");
+  configure_i2c_gpio();
+  configure_i2c_wire_interface();
+
+  // wake_mpu();
+  // configure_gyro();
+  // configure_accel_range();
+  // Serial.println("MPU6050 initialized");
 }
 
 imu_data_t collect_imu_data(void) {
@@ -94,16 +147,16 @@ void print_roll_pitch(imu_data_t data) {
   float pitch = calculate_pitch(data.accel_x);
   float roll = calculate_roll(data.accel_y, data.accel_z);
 
-  Serial.printf("Pitch: %.2f Roll: %.2f\n", pitch, roll);
+  // Serial.printf("Pitch: %.2f Roll: %.2f\n", pitch, roll);
 }
 
 void print_raw_imu_data(imu_data_t data) {
   float temp_celsius = convert_temp(data.temp_raw);
 
-  Serial.printf(
-      "accel: [%d %d %d] gyro: [%d %d %d] temp_raw: %d temp_c: %.2fC\n",
-      data.accel_x, data.accel_y, data.accel_z, data.gyro_x, data.gyro_y,
-      data.gyro_z, data.temp_raw, temp_celsius);
+  // Serial.printf(
+  //     "accel: [%d %d %d] gyro: [%d %d %d] temp_raw: %d temp_c: %.2fC\n",
+  //     data.accel_x, data.accel_y, data.accel_z, data.gyro_x, data.gyro_y,
+  //     data.gyro_z, data.temp_raw, temp_celsius);
 }
 
 void read_imu_accel_data(void) {
@@ -113,7 +166,7 @@ void read_imu_accel_data(void) {
 
   size_t written_bytes = Wire.requestFrom(MPU_ADDR, READ_BUF_SIZE);
   if (written_bytes == 0) {
-    Serial.println("Read bytes error");
+    // Serial.println("Read bytes error");
   }
 
   imu_data_t data = collect_imu_data();
