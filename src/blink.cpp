e0eacfd4207f1cb09176957bba68046804e55098
Brendan P. <64713032+brendisurfs@users.noreply.github.com>
2026-08-08 17:13:00 -0700

    fix(blink): convert blink to stm32 code
    Signed-off-by: Brendan P. <64713032+brendisurfs@users.noreply.github.com>

--- src/blink.cpp
+++ src/blink.cpp
@@ -1,43 +1,39 @@
-#include <Arduino.h>
 
-/**
- * Different flashes for different states of the system.
- */
-enum FlashDelay {
-  //  200ms for warming up
-  WARMUP = 200,
-  // 20 ms flash timeout
-  FLASH_SUCCESS = 20,
-};
+#include <stm32f411xe.h>
+#include <stm32f4xx_hal.h>
 
-void configure_led(void) {
-  pinMode(LED_BUILTIN, OUTPUT);
+#include "stm32f4xx_hal_gpio.h"
+
+#include "blink.h"
+
+#define LED_PIN GPIO_PIN_13
+#define LED_PORT GPIOC
+
+void led_gpio_init(void) {
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+
+  GPIO_InitTypeDef gpio_config{};
+  gpio_config.Pin = LED_PIN;
+  gpio_config.Pull = GPIO_NOPULL;
+  gpio_config.Mode = GPIO_MODE_OUTPUT_PP;
+  gpio_config.Speed = GPIO_SPEED_FREQ_LOW;
+
+  HAL_GPIO_Init(LED_PORT, &gpio_config);
 }
 
-void blink_led(void) {
-  digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-}
-
-/**
- * Blinks at a slower, consistent rate
- * to show that the GPS is warming up.
- */
 void gps_warmup_blink(void) {
-  blink_led();
-  delay(WARMUP);
+  HAL_GPIO_WritePin(LED_PORT, LED_PIN, GPIO_PIN_RESET);
+  HAL_Delay(BLINK_WARMUP);
+  HAL_GPIO_WritePin(LED_PORT, LED_PIN, GPIO_PIN_SET);
+  HAL_Delay(BLINK_WARMUP);
 }
 
-/**
- * Flashes the LED when the GPS has successfully
- * warmed up and connected.
- */
 void gps_connected_blink(void) {
   int max_flashes = 10;
   for (int i = 0; i < max_flashes; i++) {
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(FLASH_SUCCESS);
-    digitalWrite(LED_BUILTIN, LOW);
+    HAL_GPIO_WritePin(LED_PORT, LED_PIN, GPIO_PIN_RESET);
+    HAL_Delay(BLINK_SUCCESS);
+    HAL_GPIO_WritePin(LED_PORT, LED_PIN, GPIO_PIN_SET);
+    HAL_Delay(BLINK_SUCCESS);
   }
 }
-
-void imu_setup_success(void) {}
