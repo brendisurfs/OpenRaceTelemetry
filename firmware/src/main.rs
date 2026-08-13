65b9eff7d4776dcf08cb6a79723c6dfe0326e268
Brendan P. <64713032+brendisurfs@users.noreply.github.com>
2026-08-13 14:01:15 -0700

    fix: move the build.rs to the firmware dir, updated Cargo.toml and firmware entry
    Signed-off-by: Brendan P. <64713032+brendisurfs@users.noreply.github.com>

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -1,5 +1,29 @@
 #![no_std]
 #![no_main]
 
-#[embassy::exe]
-async fn main() -> ! {}
+use defmt::info;
+use defmt_rtt as _;
+use embassy_stm32::{
+    Config,
+    gpio::{Level, Output, Speed},
+};
+use embassy_time::Timer;
+use panic_probe as _;
+
+#[embassy_executor::main]
+async fn main(_spawner: embassy_executor::Spawner) -> ! {
+    let config = Config::default();
+    let peri = embassy_stm32::init(config);
+
+    let mut led = Output::new(peri.PC13, Level::High, Speed::Low);
+
+    loop {
+        info!("hi");
+        led.set_high();
+
+        Timer::after_millis(100).await;
+
+        led.set_low();
+        Timer::after_secs(1).await;
+    }
+}
