ab54f5321836b7aeb46a7fa3e25b6b958db94786
Brendan P. <64713032+brendisurfs@users.noreply.github.com>
2026-08-15 11:55:55 -0700

    fix(main): updated bind interrupts and buffer type error
    I read through the DMA section of the STM32F11 reference manual to check
    how the bind interrupts should be implemented. Also, fixed an issue
    where I missed the initial value to fill the buffer with.
    
    Signed-off-by: Brendan P. <64713032+brendisurfs@users.noreply.github.com>

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -1,3 +1,4 @@
+#![allow(unused)]
 #![no_std]
 #![no_main]
 
@@ -5,6 +6,7 @@ use defmt_rtt as _;
 
 use defmt::*;
 use embassy_executor::Spawner;
+use embassy_stm32::dma::InterruptHandler as DmaInterruptHandler;
 use embassy_stm32::i2c::I2c;
 use embassy_stm32::time::Hertz;
 use embassy_stm32::usart::{self, Uart};
@@ -23,10 +25,17 @@ use imu::Imu;
 const BLINK_INTERVAL: Duration = Duration::from_millis(400);
 const I2C_FREQUENCY: Hertz = Hertz(400_000);
 
+// reference: https://www.st.com/resource/en/reference_manual/rm0383-stm32f411xce-advanced-armbased-32bit-mcus-stmicroelectronics.pdf
 bind_interrupts!(struct Irqs {
     I2C1_EV => i2c::EventInterruptHandler<peripherals::I2C1>;
     I2C1_ER => i2c::ErrorInterruptHandler<peripherals::I2C1>;
     USART1 => usart::InterruptHandler<peripherals::USART1>;
+    // DMA_CHn type is pinned to stream n, so the stream number in the interrupt name should match the channel number in the type.
+    DMA1_STREAM0 => DmaInterruptHandler<peripherals::DMA1_CH0>; // I2C1_RX
+    DMA1_STREAM6 => DmaInterruptHandler<peripherals::DMA1_CH6>; // I2C1_TX
+    // This is for the USART stream
+    DMA2_STREAM7 => DmaInterruptHandler<peripherals::DMA2_CH7>; // USART1_TX
+    DMA2_STREAM2 => DmaInterruptHandler<peripherals::DMA2_CH2>; // USART1_RX
 });
 
 #[embassy_executor::main]
@@ -34,7 +43,7 @@ async fn main(spawner: embassy_executor::Spawner) -> ! {
     let peri = embassy_stm32::init(Config::default());
 
     info!("ORT firmware started");
-    let led = Led::new(peri.PC13);
+    let mut led = Led::new(peri.PC13);
 
     // IMU setup
     let i2c_config = i2c::Config::default();
@@ -66,9 +75,10 @@ async fn main(spawner: embassy_executor::Spawner) -> ! {
 
     let mut gps = Gps::new(uart);
 
-    let mut buf = [u8; 82];
+    let mut buf = [0u8; 82];
+    gps.read_sentence(&mut buf);
+
     loop {
-        unwrap!(gps.read_sentence(&mut buf));
         blink::gps_warmup_blink(&mut led);
     }
 }
